2e7dca91bb97b1ec241362517029f24f9799bdc9
Ali Aliev <ali.aliev.msk>
2022-07-13 17:22:00 +0300

    first commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 # Задание 1
 
 cache = dict()
+list_of_cache = []
 
 
 def decorator1(function):
@@ -11,12 +12,16 @@ def decorator1(function):
         intro = f'{x}, {y}'
         if intro not in cache:
             cache[intro] = function(x, y)
+            if len(list_of_cache) >= 3:
+                cache.pop(list_of_cache[0])
+                list_of_cache.__delitem__(0)
+            list_of_cache.append(intro)
             sum = cache[intro]
-            print(sum)
+            return sum
 
         else:
             sum = cache[intro]
-            print(sum)
+            return sum
 
     return wrapper
 
@@ -29,8 +34,12 @@ def function(x, y):
 decorator1(function(5, 5))
 decorator1(function(25, 5))
 decorator1(function(7, 5))
+print(cache)
+print(list_of_cache)
+decorator1(function(8, 5))
 
 print(cache)
+print(list_of_cache)
 
 
 
